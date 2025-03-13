94522b3e875ed7cec9e0865d9a5a628a1a20a765
Tien Nguyen <tien96ng@gmail.com>
2025-03-12 23:32:57 -0700

    Update ping.js to perform random quote from arr

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,8 +3,23 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('lakers')
-		.setDescription('Replies with `LAKERS in 5!`'),
+		.setDescription('Replies with random Lakers in 5 Joke'),
 	async execute(interaction) {
-		await interaction.reply('LAKERS in 5!');
+		await interaction.reply(randomQuote());
 	},
 };
+
+let quoteArr = [
+    "serge ibaka, chewbacca, waka flocka",
+    "Jayson Tatum, Jason Statham, Jason Bateman",
+    "OKC, UFC, KFC",
+    "Jayson Tatum, Channing Tatum, Andrew Tate",
+    "Stephen Curry, Tempero Cury, Mc Flury",
+    "Celtics, Jellyfish, Hurricane Chris",
+    "Let it shine, Porcupine, Lemon Lime",
+    "Steph Threes, Embiids Kness, Austin Reaves",
+    "SGA, PGA, USA",
+    "Jimmy Tran, Matthew Tran, Tranny Trans"
+]
+
+const randomQuote = () => quoteArr[Math.floor(Math.random() * quoteArr.length)].concat(" ... LAKERS IN 5!");
