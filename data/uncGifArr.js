236ce8029b64b74bdfb4e29f3f51bda79277fce0
Tien Nguyen <tien96ng@gmail.com>
2025-03-13 19:55:58 -0700

    Create arrays for random rythming quotes and gifs of Shannon Sharpe being an unc in their own seperate .js file with exports

---
+++ data/uncGifArr.js
@@ -0,0 +1,21 @@
+const uncGifArr = [
+    "https://media1.tenor.com/m/h-fQB_FC578AAAAC/shannon-sharpe-shay",
+    "https://tenor.com/view/shannon-sharpe-shannon-sharpe-mountain-dew-shannon-sharpe-drinking-mountain-dew-gif-8003291754061172810",
+    "https://tenor.com/view/shannon-sharpe-gif-23617913",
+    "https://tenor.com/view/shannon-sharpe-undisputed-excuse-me-looking-gif-12574883",
+    "https://tenor.com/view/shannon-sharpe-undisputed-no-prob-gif-12408893",
+    "https://tenor.com/view/shannon-sharpe-gif-15825448190899412080",
+    "https://tenor.com/view/shannon-sharpe-no-nope-nah-impossible-gif-13119524",
+    "https://tenor.com/view/shannon-shannon-sharpe-lol-laughing-funny-gif-17814021",
+    "https://tenor.com/view/shannon-sharpe-thinking-gif-13199263",
+    "https://tenor.com/view/shannon-sharpe-funny-gif-14419764",
+    "https://tenor.com/view/iou-gif-25469529",
+    "https://tenor.com/view/shannon-sharpe-drink-sip-gif-12229612",
+    "https://tenor.com/view/shannon-sharpe-shay-shay-monique-uncle-shay-ikyl-gif-5528589471884230087",
+    "https://tenor.com/view/shannon-sharpe-huh-what-que-confused-gif-9249931",
+    "https://tenor.com/view/club-shay-shay-clubshayshay-club-shayshay-shannon-sharpe-keke-palmer-gif-2795832499264296299",
+    "https://tenor.com/view/no-shannon-sharpe-stop-it-undisputed-shocked-gif-15169546",
+    "https://tenor.com/view/shannon-sharpe-skip-bayless-gif-12516770"
+];
+
+module.exports = uncGifArr;
