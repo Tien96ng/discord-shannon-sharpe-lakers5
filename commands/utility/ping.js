ec0d8e6c51e9006d7cad6f7ddab15121e76cfe7b
Tien Nguyen <tien96ng@gmail.com>
2025-03-13 19:57:35 -0700

    Create functions to generate random quotes and random gifs from each specificed array and feed it to the interaction slash reply payload

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,25 +1,21 @@
 const { SlashCommandBuilder } = require('discord.js');
+const quoteArr = require("../../data/quoteArr");
+const uncGifArr = require("../../data/uncGifArr")
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('lakers')
-		.setDescription('Replies with random Lakers in 5 Joke'),
+		.setDescription('Replies with random Lakers in 5 Joke rhyming scheme with a random gif of unc Shannon Sharpe.'),
 	async execute(interaction) {
-		await interaction.reply(randomQuote());
+		// await interaction.reply(randomQuote());
+        await interaction.reply({
+            content: randomQuote(),
+            files: [randomGif()] // Direct image URL
+        });
 	},
 };
 
-let quoteArr = [
-    "serge ibaka, chewbacca, waka flocka",
-    "Jayson Tatum, Jason Statham, Jason Bateman",
-    "OKC, UFC, KFC",
-    "Jayson Tatum, Channing Tatum, Andrew Tate",
-    "Stephen Curry, Tempero Cury, Mc Flury",
-    "Celtics, Jellyfish, Hurricane Chris",
-    "Let it shine, Porcupine, Lemon Lime",
-    "Steph Threes, Embiids Kness, Austin Reaves",
-    "SGA, PGA, USA",
-    "Jimmy Tran, Matthew Tran, Tranny Trans"
-]
+const rndNum = (arr) => Math.floor(Math.random() * arr.length);
 
-const randomQuote = () => quoteArr[Math.floor(Math.random() * quoteArr.length)].concat(" ... LAKERS IN 5!");
+const randomQuote = () => quoteArr[rndNum(quoteArr)].join(", ").concat(" ... \nLAKERS IN 5!");
+const randomGif = () => uncGifArr[rndNum(uncGifArr)].concat(".gif");
